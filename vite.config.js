9e5c4592d777711700f8d12d4e3d93c9e7bdfda3
Beybit <eshimkul0&@mail.ru>
2026-02-07 00:26:11 +0500

    disable vue devtools in production

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,14 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// ❗ devtools ТОЛЬКО локально
+const plugins = [vue()]
+
+if (process.env.NODE_ENV !== 'production') {
+  const { default: vueDevtools } = await import('vite-plugin-vue-devtools')
+  plugins.push(vueDevtools())
+}
+
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+  plugins
 })
